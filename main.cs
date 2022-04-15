8e2ca5212688485c60e3ab6e00f05464590a68d4
ACIDULATED <93052902+ACIDULATED@users.noreply.github.com>
2022-04-15 09:47:47 -0400

    The first commit!

---
+++ main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        String[] strings = new string[999];
+        int num = 0;
+        Console.WriteLine("Welcome to Lazy#! Enter your code here.\n");
+        while (true)
+        {
+            string s = Console.ReadLine();
+            if (s.StartsWith("write "))
+            {
+               if (s.Substring(6).StartsWith("TXT")) {
+                    Console.WriteLine(strings[Convert.ToInt32(s.Substring(9))]);
+               }
+            }
+            else if (s.StartsWith("text "))
+            {
+                strings[num] = s.Substring(5);
+                Console.WriteLine("String " + num.ToString() + " is now " + strings[num]);
+            }
+        }
+    }
+}
