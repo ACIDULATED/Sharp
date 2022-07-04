bd017eabc644c8ce00d93e45e414df08d6602762
ACIDULATED <93052902+ACIDULATED@users.noreply.github.com>
2022-07-04 15:58:22 -0400

    lovr

--- Sharp/main.cs
+++ Sharp/main.cs
@@ -172,5 +172,117 @@ class Program
             Console.WriteLine("Done!");
             Console.ReadLine();
         }
+
+        else if (args[0] == "-lovr")
+        {
+            Console.WriteLine("Drag your folder in and press enter.");
+            string path = Console.ReadLine();
+            string[] lines = File.ReadAllLines(path + @"\load.shr");
+            StreamWriter sw = File.CreateText(path + @"\main.lua");
+            sw.WriteLine("function lovr.load");
+            foreach (string s in lines)
+            {
+                var sections = s.Split(' ');
+                switch (sections[0])
+                {
+                    case "write":
+                        if (sections[1].StartsWith("TXT"))
+                        {
+                            sw.WriteLine("lovr.graphics.print(" + strings[Convert.ToInt32(s.Substring(9))] + ", 0, 0, 0, 0, 0, 0, 0)");
+                        }
+                        else
+                        {
+                            sw.WriteLine("lovr.graphics.print(" + sections[1] + ", 0, 0, 0, 0, 0, 0, 0)");
+                        }
+                        break;
+                    case "text":
+                        strings[num] = sections[1];
+                        break;
+                    case "rect":
+                        // rect [color] [x] [y] [w] [h]
+                        break;
+                    case "circle":
+                        // draw a circle
+                        break;
+                    case "3d_model":
+                        sw.WriteLine(sections[1] + " = lovr.graphics.newModel('" + sections[2] + "')");
+                        break;
+                }
+            }
+            sw.WriteLine("end");
+
+            lines = File.ReadAllLines(path + @"\update.shr");
+            sw.WriteLine("function love.update");
+            foreach (string s in lines)
+            {
+                var sections = s.Split(' ');
+                switch (sections[0])
+                {
+                    case "write":
+                        if (sections[1].StartsWith("TXT"))
+                        {
+                            sw.WriteLine("lovr.graphics.print(" + strings[Convert.ToInt32(s.Substring(9))] + ", 0, 0, 0, 0, 0, 0, 0)");
+                        }
+                        else
+                        {
+                            sw.WriteLine("lovr.graphics.print(" + sections[1] + ", 0, 0, 0, 0, 0, 0, 0)");
+                        }
+                        break;
+                    case "text":
+                        strings[num] = sections[1];
+                        break;
+                    case "rect":
+                        // rect [color] [x] [y] [w] [h]
+                        break;
+                    case "circle":
+                        // draw a circle
+                        break;
+                    case "3d_model":
+                        sw.WriteLine(sections[1] + " = lovr.graphics.newModel('" + sections[2] + "')");
+                        break;
+                }
+            }
+            sw.WriteLine("end");
+
+            lines = File.ReadAllLines(path + @"\draw.shr");
+            sw.WriteLine("function love.draw");
+            foreach (string s in lines)
+            {
+                var sections = s.Split(' ');
+                switch (sections[0])
+                {
+                    case "write":
+                        if (sections[1].StartsWith("TXT"))
+                        {
+                            sw.WriteLine(@"lovr.graphics.print(""" + strings[Convert.ToInt32(s.Substring(9))] + @"""" + ", 0, 0, 0, 0, 0, 0, 0)");
+                        }
+                        else
+                        {
+                            sw.WriteLine(@"lovr.graphics.print(""" + sections[1] + @"""" + ", 0, 0, 0, 0, 0, 0, 0)");
+                        }
+                        break;
+                    case "text":
+                        strings[num] = sections[1];
+                        break;
+                    case "rect":
+                        // rect [color] [x] [y] [w] [h]
+                        break;
+                    case "circle":
+                        // draw a circle
+                        break;
+                    case "3d_model":
+                        sw.WriteLine(sections[1] + " = lovr.graphics.newModel('" + sections[2] + "')");
+                        break;
+                    case "draw_model":
+                        sw.WriteLine(sections[1] + ":draw(" + sections[2] + ", " + sections[3] + ", " + sections[4] + ", " + sections[5] + ", " + sections[5] + ")");
+                        break;
+                }
+            }
+            sw.WriteLine("end");
+            sw.Flush();
+            sw.Close();
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
     }
 }
